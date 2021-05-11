14171fbd2f2e52948b04d6727434eb2bdf67cbc2
george <yuanshuai@qiaokr.com>
2021-05-11 17:03:07 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,4 +28,11 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
         }
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flush('success','您已成功退出！');
+        return redirect('login');
+    }
 }
