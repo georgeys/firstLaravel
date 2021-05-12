5e7a09bde77362b08eea47310a5ae266e4d32b2b
george <yuanshuai@qiaokr.com>
2021-05-12 16:59:15 +0800

    用户激活

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class UsersController extends Controller
 {
@@ -12,12 +13,12 @@ class UsersController extends Controller
     public function __construct()
     {
         //中间件‘auth’为名称 ‘except’排除/‘only’指定方法
-        $this->middleware('auth',[
-            'except' => ['show','create','store','index']
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index','confirmEmail']
         ]);
 
         //仅允许未登录的访问注册页面
-        $this->middleware('guest',[
+        $this->middleware('guest', [
             'only' => ['create']
         ]);
     }
@@ -34,7 +35,7 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-    //登录
+    //创建
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -49,49 +50,80 @@ class UsersController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
-        Auth::login($user);
-        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show',[$user]);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+        return redirect('/');
     }
 
     public function edit(User $user)
     {
         $this->authorize('update', $user);
-        return view('users.edit',compact('user'));
+        return view('users.edit', compact('user'));
     }
 
-    public function update(User $user,Request $request)
+    public function update(User $user, Request $request)
     {
         $this->authorize('update', $user);
-        $this->validate($request,[
+        $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'required|confirmed|min:6'
         ]);
 
         $data = [];
         $data['name'] = $request->name;
-        if ($request->password){
+        if ($request->password) {
             $data['password'] = bcrypt($request->password);
         }
 
         $user->update($data);
 
-        session()->flash('success','个人资料更新成功！');
+        session()->flash('success', '个人资料更新成功！');
 
-        return redirect()->route('users.show',$user->id);
+        return redirect()->route('users.show', $user->id);
     }
 
     public function index()
     {
         $users = User::paginate(10);
-        return view('users.index',compact('users'));
+        return view('users.index', compact('users'));
     }
 
     public function destroy(User $user)
     {
-        $this->authorize('destroy',$user);
+        $this->authorize('destroy', $user);
         $user->delete();
-        session()->flash('success','删除ok');
+        session()->flash('success', '删除ok');
         return back();
     }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success','恭喜，激活成功');
+
+        return redirect()->route('users.show',[$user]);
+
+    }
+
+    public function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'summer@example.com';
+        $name = 'Summer';
+        $to = $user->email;
+        $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
+        Mail::send($view,$data,function ($message) use ($from,$name,$to,$subject){
+
+            $message->from($from,$name)->to($to)->subject($subject);
+
+        });
+    }
+
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -11,6 +11,7 @@
 <div class="container">
     <div class="offset-md-1 col-md-10">
         @include('shared._messages')
+        @include('shared._errors')
         @yield('content')
         @include('layouts._footer')
     </div>
